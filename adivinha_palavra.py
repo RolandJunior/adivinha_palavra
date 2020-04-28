c567aa595e622342903c79b8de03174cc563cf2a
Roland B. Junior <roland.junior@outlook.com>
2020-04-28 19:40:30 -0300

    Versão 0.2
    Versão com uso de funções e mais eficiente.

--- adivinha_palavra.py
+++ adivinha_palavra.py
@@ -1,58 +1,46 @@
 # -*- coding: utf-8 -*-
 
-import random, time
+import random
+from collections import Counter
 
-# Carrega alfabeto e gera dict para contagem de letras
-with open('alfabeto.txt') as arquivo:
-  alfabeto = dict.fromkeys(arquivo.read().splitlines(),0)
+def seleciona_letra(dic):
+    return Counter(''.join(dic)).most_common(1)[0][0]
+    # Converte dicionário numa string contínua (join)
+    # Pega a letra mais comum (Counter.most_common)
+    # Retorno é uma lista de tuplas (letra,quant) - no caso uma só
+    # Separa letra da tupla ([0][0])
+##### ADICIONAR VERIFICAÇÃO DE LETRAS JÁ USADAS
 
-# Carrega dicionário
+def testa_letra(letra):
+    if palavra.find(letra) == -1: #Marca letra errada
+        erros += 1
+        erradas.append(letra)
+    else: #Acha posição de letra certa
+        for caracter in range(num_caracteres):
+            if letra == palavra[caracter]:
+                resultado[caracter] = letra
+    print(resultado)
+
 with open('dicionario.txt') as arquivo:
-  dicionario = arquivo.read().splitlines()
-  p = f'{len(dicionario):,}'.replace(',','.') 
+    dicionario = arquivo.read().splitlines()
 
-# Abertura do teste
-print(f'\nO computador tentará adivinhar uma palavra escolhida aleatoriamente de um dicionário de {p} palavras.')
-print('A única informação que ele receberá será o número de caracteres da palavra.\n')
-x = input('Tecle ENTER para gerar a palavra:')
-
-# Seleciona aleatoriamente uma palavra do dicionário e calcula seu número de caracteres
 palavra = random.choice(dicionario)
 num_caracteres = len(palavra)
-print(f'\nA palavra escolhida foi "{palavra}"" e ela tem {num_caracteres} caracteres.\n')
-x = input('Tecle ENTER para começar a adivinhação:')
 
-# Inicia timer
-t1 = time.perf_counter()
-
-# Filtra as que tem o número de caracteres correto
 dic_filtrado = [i for i in dicionario if len(i) == num_caracteres]
 
-# Conta letras em todas as palavras e ordena alfabeto da maior pra menor quantidade
-for i in dic_filtrado:
-  for j in alfabeto:
-    alfabeto[j] += i.count(j)
-alfabeto_ordenado = {k:v for k,v in sorted(alfabeto.items(), key=lambda item: item[1], reverse=True)}
-
-# Identifica palavra
-c = '_'
-resultado = list(c * num_caracteres)
 tentativas = erros = 0
 erradas = []
-for letra in alfabeto_ordenado:
-  if resultado.count(c) == 0: #Sai se já achou todas as letras
-    break
-  tentativas += 1
-  if palavra.find(letra) == -1: #Marca letra errada
-    erros += 1
-    erradas.append(letra)
-  else: #Acha posição de letra certa
-    for caracter in range(num_caracteres):
-      if letra == palavra[caracter]:
-        resultado[caracter] = letra
-resultado = ''.join(resultado)
-t2 = time.perf_counter()
-print(f'\nA palavra é "{resultado}"')
-print(f'O programa gastou {t2-t1:.4f} segundos para adivinhá-la.')
-print(f'Foram testadas {tentativas} letras e {erros} estavam erradas: {", ".join(erradas)}')
+c = '_'
+resultado = list(c * num_caracteres)
+#while resultado.count(c) > 0:
+    #tentativas += 1
+    #seleciona_letra()
+    #testa_letra()
+    #limpa_dicionario()
 
+l = seleciona_letra(dic_filtrado)
+testa_letra(l)
+print(palavra, l)
+
+#resultado = ''.join(resultado)
