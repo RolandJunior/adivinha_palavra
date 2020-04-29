fac2523ea664728a00f10b7c93135cf68b468f5c
Roland B. Junior <roland.junior@outlook.com>
2020-04-29 18:11:28 -0300

    v0.2
    Novo código com uso de funções e sem necessidade de alfabeto.

--- adivinha_palavra.py
+++ adivinha_palavra.py
@@ -1,46 +1,53 @@
 # -*- coding: utf-8 -*-
 
-import random
+import random, time
 from collections import Counter
 
 def seleciona_letra(dic):
-    return Counter(''.join(dic)).most_common(1)[0][0]
-    # Converte dicionário numa string contínua (join)
-    # Pega a letra mais comum (Counter.most_common)
-    # Retorno é uma lista de tuplas (letra,quant) - no caso uma só
-    # Separa letra da tupla ([0][0])
-##### ADICIONAR VERIFICAÇÃO DE LETRAS JÁ USADAS
+    x = len(testadas)
+    return Counter(''.join(dic)).most_common(x+1)[x][0]
 
 def testa_letra(letra):
-    if palavra.find(letra) == -1: #Marca letra errada
-        erros += 1
+    testadas.append(letra)
+    if palavra.find(letra) == -1:
         erradas.append(letra)
-    else: #Acha posição de letra certa
-        for caracter in range(num_caracteres):
-            if letra == palavra[caracter]:
-                resultado[caracter] = letra
-    print(resultado)
+    else:
+        for pos in range(num_caracteres):
+            if letra == palavra[pos]:
+                resultado[pos] = letra
 
+# Carrega dicionário e conta palavras
 with open('dicionario.txt') as arquivo:
     dicionario = arquivo.read().splitlines()
+    p = f'{len(dicionario):,}'.replace(',','.') 
 
+# Abertura do teste
+print(f'\nO computador tentará adivinhar uma palavra escolhida aleatoriamente de um dicionário de {p} palavras.')
+print('A única informação que ele receberá será o número de caracteres da palavra.')
+x = input('Tecle ENTER para gerar a palavra:')
+
+# Seleciona aleatoriamente uma palavra do dicionário e calcula seu número de caracteres
 palavra = random.choice(dicionario)
 num_caracteres = len(palavra)
+print(f'A palavra escolhida foi "{palavra}"" e ela tem {num_caracteres} caracteres.')
+x = input('Tecle ENTER para começar a adivinhação:')
 
+# Inicia timer
+t1 = time.perf_counter()
+
+# Filtra as palavras que tem o número de caracteres correto
 dic_filtrado = [i for i in dicionario if len(i) == num_caracteres]
 
-tentativas = erros = 0
+# Identifica a palavra
+testadas = []
 erradas = []
-c = '_'
-resultado = list(c * num_caracteres)
-#while resultado.count(c) > 0:
-    #tentativas += 1
-    #seleciona_letra()
-    #testa_letra()
-    #limpa_dicionario()
+resultado = list('_' * num_caracteres)
+while resultado.count('_') > 0:
+    testa_letra(seleciona_letra(dic_filtrado))
 
-l = seleciona_letra(dic_filtrado)
-testa_letra(l)
-print(palavra, l)
-
-#resultado = ''.join(resultado)
+# Mostra resultados
+resultado = ''.join(resultado)
+t2 = time.perf_counter()
+print(f'A palavra é "{resultado}"')
+print(f'O programa gastou {t2-t1:.4f} segundos para adivinhá-la.')
+print(f'Foram testadas {len(testadas)} letras e {len(erradas)} estavam erradas: {", ".join(erradas)}')
