f740344d91677fe0c369d2b78e6de72e73cdefa6
Roland B. Junior <roland.junior@outlook.com>
2020-05-07 11:33:51 -0300

    Add files via upload
    Adicionada funções à classe Dicionario

--- adivinha_palavra.py
+++ adivinha_palavra.py
@@ -5,8 +5,20 @@ from collections import Counter
 
 class Dicionario():
     def __init__(self, arquivo):
-        self.palavras = arquivo.read().splitlines()
-        self.quantidade = len(self.palavras)
+        self.lista = arquivo.read().splitlines()
+        
+    def conta_palavras(self):    
+        return len(self.lista)
+    
+    def escolhe_palavra(self):
+        return random.choice(self.lista)
+    
+    def filtra_dic_len(self, carac):
+        self.lista = [i for i in self.lista if len(i) == carac]
+        
+    def filtra_dic_errada(self, letra_errada):
+        self.lista = [i for i in self.lista if letra_errada not in i]
+
 
 def seleciona_letra(dic):
     x = 0
@@ -27,10 +39,11 @@ def testa_letra(l):
                 resultado[pos] = l
         return 0
 
+
 # Carrega dicionário e conta palavras
 with open('dicionario.txt') as arquivo:
     dic = Dicionario(arquivo)
-quant = f'{dic.quantidade:,}'.replace(',','.') 
+quant = f'{dic.conta_palavras():,}'.replace(',','.') 
 
 # Abertura do teste
 print(f'\nO computador tentará adivinhar uma palavra escolhida aleatoriamente de um dicionário de {quant} palavras.')
@@ -38,7 +51,7 @@ print('A única informação que ele receberá será o número de caracteres da 
 x = input('Tecle ENTER para gerar a palavra:')
 
 # Seleciona aleatoriamente uma palavra do dicionário e calcula seu número de caracteres
-palavra = random.choice(dic.palavras)
+palavra = dic.escolhe_palavra()
 num_caracteres = len(palavra)
 print(f'A palavra escolhida foi "{palavra}"" e ela tem {num_caracteres} caracteres.')
 x = input('Tecle ENTER para começar a adivinhação:')
@@ -47,18 +60,18 @@ x = input('Tecle ENTER para começar a adivinhação:')
 t1 = time.perf_counter()
 
 # Filtra as palavras que tem o número de caracteres correto
-dic_filtrado = [i for i in dic.palavras if len(i) == num_caracteres]
+dic.filtra_dic_len(num_caracteres)
 
 # Identifica a palavra
 testadas = []
 erradas = []
 resultado = list('_' * num_caracteres)
 while resultado.count('_') > 0:
-    letra = seleciona_letra(dic_filtrado)
+    letra = seleciona_letra(dic.lista)
     letra_stat = testa_letra(letra)
     # Remove do dicionário palavras com letras erradas
     if letra_stat == 1: 
-        dic_filtrado = [i for i in dic_filtrado if letra not in i]
+        dic.filtra_dic_errada(letra)
 
 # Mostra resultados
 resultado = ''.join(resultado)
